1e80818ff89a0a3dcbf53cb3c1fb00a2eb70784a
Aydin Yakar <yakar@live.com>
2017-08-23 22:06:07 +0300

    google-authentication-installer.sh: check running with sudo and sshd_config fixed

--- google-authentication-installer.sh
+++ google-authentication-installer.sh
@@ -7,14 +7,19 @@
 # 2FA with Google Authenticator
 
 # install pam library
-sudo apt-get -y install libpam-google-authenticator
+set -e
+
+# root check
+if [ "$(id -u)" != "0" ]; then echo "Run with sudo: sudo $0"; exit 1; fi
+
+apt-get -y install libpam-google-authenticator
 
 if [ grep -vq "Enable MFA using Google Authenticator PAM" /etc/pam.d/sshd ]; then
 	echo -e '\n# Enable MFA using Google Authenticator PAM\nauth required pam_google_authenticator.so nullok' >> /etc/pam.d/sshd
 
-	sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g"
+	sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g" /etc/ssh/sshd_config
 
-	sudo service ssh restart
+	service ssh restart
 
 	read -p "Do yo want to enable 2FA to the local login? y/n " cevap
 	case $cevap in
